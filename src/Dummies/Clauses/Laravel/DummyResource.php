e7d50bb7468fea9e295b6c95e222a8f686af461d
Marc Eichenseher <eichenseher.marc@gmail.com>
2019-12-14 02:51:50 +0100

    Ignore third party dummies.

--- src/Dummies/Clauses/Laravel/DummyResource.php
+++ src/Dummies/Clauses/Laravel/DummyResource.php
@@ -5,6 +5,13 @@ namespace Ieim\LaravelContracts\Dummies\Clauses\Laravel;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * Class DummyResource
+ * @package Ieim\LaravelContracts\Dummies\Clauses\Laravel
+ *
+ * We dont test third party code.
+ * @codeCoverageIgnore
+ */
 class DummyResource extends JsonResource
 {
     /**
